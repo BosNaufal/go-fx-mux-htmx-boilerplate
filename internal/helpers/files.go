a86217e9898e82ecb8401f9d25246d88611ad631
Naufal Rabbani <bosnaufalemail@gmail.com>
2026-08-18 22:55:50 +0700

    Feat: add basic page with HTMX and Jquery (#2)
    * fix: remove unused file
    
    * fix: rename dir
    
    * fix: remove comment
    
    * feat: organize template helpers
    
    * feat: make the function smaller hopefully more maintainable

---
+++ internal/helpers/files.go
@@ -0,0 +1,46 @@
+package helpers
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/repeale/fp-go"
+)
+
+type SimpleDirEntry struct {
+	FullPath string
+	Name     string
+	isDir    bool
+}
+
+func simplifyOsDirEntry(dir string, entry os.DirEntry) SimpleDirEntry {
+	return SimpleDirEntry{
+		FullPath: filepath.Join(dir, entry.Name()),
+		Name:     entry.Name(),
+		isDir:    entry.IsDir(),
+	}
+}
+
+func readDirSimple(dir string) []SimpleDirEntry {
+	osEntries, _ := os.ReadDir(dir)
+	simplifyOsDirEntryCurrDir := fp.Curry2(simplifyOsDirEntry)(dir)
+	return fp.Map(simplifyOsDirEntryCurrDir)(osEntries)
+}
+
+func WalkingReadDir(dir string) []SimpleDirEntry {
+	simpleEntries := readDirSimple(dir)
+	return fp.Reduce(func(acc []SimpleDirEntry, curr SimpleDirEntry) []SimpleDirEntry {
+		// Recursive read the dir with output SimpleDirEntry{}
+		if curr.isDir {
+			nextDirToScan := filepath.Join(dir, curr.Name)
+			return append(acc, WalkingReadDir(nextDirToScan)...)
+		}
+
+		// Return all the file list if there's no dir.
+		return append(acc, curr)
+	}, []SimpleDirEntry{})(simpleEntries)
+}
+
+func SimpleDirEntryToFilePathString(dirEntry SimpleDirEntry) string {
+	return dirEntry.FullPath
+}
---
+++ internal/helpers/template.go
@@ -0,0 +1,33 @@
+package helpers
+
+import (
+	"html/template"
+	"io"
+	"path/filepath"
+
+	"github.com/repeale/fp-go"
+)
+
+func parseTemplateFiles(filepaths []string) *template.Template {
+	tmpl := template.Must(template.ParseFiles(filepaths...))
+	return tmpl
+}
+
+func renderPageHTML(absolutePathToPageHTML string) *template.Template {
+	var layoutDir, _ = filepath.Abs("templates/layouts")
+
+	layoutFiles := fp.Pipe2(
+		WalkingReadDir,
+		fp.Map(SimpleDirEntryToFilePathString),
+	)(layoutDir)
+
+	var pageTargetPath, _ = filepath.Abs(absolutePathToPageHTML)
+	var tmpl = parseTemplateFiles(append(layoutFiles, pageTargetPath))
+	return tmpl
+}
+
+func RenderPageWithLayout(writer io.Writer, layoutName string, absolutePathToPageHTML string, data any) error {
+	var tmpl = renderPageHTML(absolutePathToPageHTML)
+	err := tmpl.ExecuteTemplate(writer, layoutName, data)
+	return err
+}
