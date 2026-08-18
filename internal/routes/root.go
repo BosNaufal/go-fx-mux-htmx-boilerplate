aff2414a622717835b7a84b098592841cdd7c6ce
Naufal Rabbani <bosnaufalemail@gmail.com>
2026-08-18 22:20:37 +0700

    fix: remove comment

--- internal/routes/root.go
+++ internal/routes/root.go
@@ -11,26 +11,6 @@ import (
 	"github.com/repeale/fp-go"
 )
 
-// func fpGoUsecase() {
-// 	// Create an Option
-// 	some := option.Some(42)
-// 	none := option.None[int]()
-
-// 	// Map over values
-// 	doubled := option.Map(N.Mul(2))(some)
-// 	fmt.Println(option.GetOrElse(lazy.Of(0))(doubled)) // Output: 84
-// 	fmt.Println(option.GetOrElse(lazy.Of(1))(none))    // Output: 1
-
-// 	// // Chain operations
-// 	result := option.Chain(func(x int) option.Option[string] {
-// 		if x > 0 {
-// 			return option.Some(fmt.Sprintf("Positive: %d", x))
-// 		}
-// 		return option.None[string]()
-// 	})(some)
-// 	fmt.Println(option.GetOrElse(lazy.Of("No value"))(result)) // Output: Positive: 42
-// }
-
 func RootPage(w http.ResponseWriter, r *http.Request) {
 	// an example API handler
 
