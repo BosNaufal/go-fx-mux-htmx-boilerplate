89f9554f9ad1b9f60f3be6cb0fedca5b96df4e77
Naufal Rabbani <bosnaufalemail@gmail.com>
2026-08-19 19:02:32 +0700

    feat: add open props and normalize css

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -1,11 +1,18 @@
 package routes
 
 import (
+	"net/http"
+	"path/filepath"
+
 	"github.com/gorilla/mux"
 )
 
 func NewRouter() *mux.Router {
 	router := mux.NewRouter()
+	// This will serve files under http://localhost:8000/static/<filename>
+	publicDir, _ := filepath.Abs("public/")
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(publicDir))))
+
 	router.HandleFunc("/api", RootResource)
 	router.HandleFunc("/", RootPage)
 	return router
